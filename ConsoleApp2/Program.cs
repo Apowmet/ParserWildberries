ae1845a3f4657a2d098775d7c0781c48731964f4
Apowmet <4ai6ezcaxapa@gmail.com>
2022-11-19 10:04:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using ParserWildberries;
+
+var searchStrings = File.ReadAllLines(@"C:\Keys.txt");
+
+var wbEngine = new WbEngine();
+List<ExcelProductsModel> excelProductsModels = new List<ExcelProductsModel>();
+
+foreach (var searchString in searchStrings)
+{
+    var products = await wbEngine.GetDataByQueryTerm(searchString);
+    excelProductsModels.Add(new ExcelProductsModel() 
+    { 
+        Products= products, 
+        SearchString = searchString 
+    });
+}
+wbEngine.WriteIntoExcel(excelProductsModels);
+
+Console.WriteLine("Done");
